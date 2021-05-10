4ed5f2ae2ad3788025bc176913fbfd333d4b4146
Sushil <58723338+sushcode5@users.noreply.github.com>
2021-05-10 17:12:41 +0530

    Create Count Smaller or Equal elements in array.cpp

---
+++ CodeStudio/Array/Count Smaller or Equal elements in array.cpp
@@ -0,0 +1,89 @@
+/*
+         Name - Rindhe Sushil Samadhan
+         IIT Guwahati
+         
+*/
+
+/*
+Problem Statement
+You are given two arrays of integers. Let's call the first array A and the second array B. For every element of array, A find the number of elements in array B which are smaller than or equal to that element.
+For Example:
+A=[2,3,0] , B=[5,1]
+For the first index, A[0]=2
+In array B only 1 is less than 2. Therefore the answer for the first index is 1.
+For the second index, A[1]=3
+In array B only 1 is less than 3. Therefore the answer for the second index is also 1.
+For the third index, A[2]=0
+Both the elements of array B are greater than 0. 
+Therefore the answer for the third index is 0.
+Hence, the final answer is [1,1,0] in this case.
+Input Format:
+The first line of input contains an integer ‘T’ denoting the number of test cases.
+Then the test case follows.
+The first line of each test case contains an integer ‘N’ denoting the number of elements in the array A. 
+The second line of each test case contains ‘N’ space-separated integers denoting the elements of array A.
+The third line of each test case contains an integer ‘M’ denoting the number of elements in the array B. 
+The second line of each test case contains ‘M' space-separated integers denoting the elements of array B.
+Output Format:
+For each test case, print N space-separated integers represent the number of elements in array B that are smaller than the corresponding element in array A.
+Constraints:
+1 <= T <= 10
+1 <= N,M <= 10^4
+-10^9 <= A[i] , B[i]<= 10^9
+
+Time limit: 1 second
+Note:
+Do not print anything, just return an array of integers denoting the number of elements in array B that are smaller than the corresponding element in an element of array A.
+Sample Input 1:
+1
+5
+5 4 3 2 1
+2
+3 4
+Sample Output 1:
+2 2 1 0 0
+Explanation Of Sample Input 1 :
+For the first index, A[0]=5
+Both the elements of Array B are less than 5. Therefore the answer for the first index is 2.
+For the second index, A[1]=4
+In array B, one element is smaller than 4 and one element is equal to 4. Therefore the answer for the second index is also 2.
+For the third index, A[2]=3
+In array B only one element is equal to 3. Therefore the answer for the third index is 1.
+For the fourth index, A[3]=2
+Both the elements of Array B are greater than 2. Therefore the answer for the fourth index is 0.
+For the fifth index, A[4]=1
+Both the elements of Array B are greater than 1. Therefore the answer for the last index is 0.
+Hence, the final answer is [2,2,1,0,0] in this case.
+Sample Input 2:
+1
+4
+-2 1 3 0
+4
+-1 0 2 1
+Sample Output 2:
+ 0 3 4 2
+
+*/
+
+
+
+// BrutForce Solution
+
+//Time Complexity - O(n*m) where n and m are size of two arrays.
+
+vector < int > countSmallerOrEqual(int * a, int * b, int n, int m) {
+    vector<int> result;
+   for(int i=0;i<n;i++){
+       int count = 0;
+       for(int j=0;j<m;j++){
+           if(b[j]<= a[i]){
+               count++;
+           }
+       }
+       result.push_back(count);
+   }
+    return result;
+}
+
+
+

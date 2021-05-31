7473c942b6896c52b5f3aadde867eebdcc15c9ea
Sushil <58723338+sushcode5@users.noreply.github.com>
2021-05-31 12:40:35 +0530

    Create fibonacci.cpp

---
+++ DP/fibonacci.cpp
@@ -0,0 +1,40 @@
+/*
+            Name - Rindhe Sushil Samadhan
+            IIT Guwahati
+            
+ */
+
+
+#include <iostream>
+using namespace std;
+// Time Complexity - O(2^N)
+int fibonacci(int n){
+    if(n==0 || n==1){
+        return 1;
+    }
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+
+// Time Complexity - O(N)
+int fib2(int n, int*arr){
+    if(n==0 || n==1){
+        return 1;
+    }
+    if(arr[n] > 0){
+        return arr[n];
+    }
+    
+    int output = fib2(n-1,arr) + fib2(n-2,arr);
+    arr[n] = output;
+    return arr[n];
+}
+
+int main() {
+	
+	int n;
+	cin >> n;
+	int *arr = new int[n]();
+	cout<<fib2(n,arr)<<endl;
+	delete[] arr;
+	return 0;
+}

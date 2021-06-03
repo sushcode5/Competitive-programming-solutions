d1984e2a76680bc94dda68f4c99732a872aa5644
Sushil <58723338+sushcode5@users.noreply.github.com>
2021-06-03 09:19:13 +0530

    Update Ways To Make Coin Change.cpp

--- DP/Ways To Make Coin Change.cpp
+++ DP/Ways To Make Coin Change.cpp
@@ -39,5 +39,24 @@ long countWaysToMakeChange(int *denominations, int n, int value)
 }
 
 
+//Iterative Solution
+// Time Complexity - O(N*V)
+// Space Complexity - O(V)
 
+long countWaysToMakeChange(int *denominations, int n, int value)
+{
+    long dp[value+1];
+    for(int i=1; i<=value;i++){
+        dp[i] = 0;
+    }
+    dp[0] = 1;
+    
+    for(int i=0;i<n;i++){
+        
+        for(int j=denominations[i]; j<=value; j++){
+            dp[j] += dp[j - denominations[i]];
+        }
+    }
+    return dp[value];
+}
 
